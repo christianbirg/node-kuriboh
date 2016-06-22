6ea92b05a923f8ba0bcbf3fd62f73dcec4ef6d72
Christian Birg <cbirg@tecracer.de>
2016-06-22 11:51:01 +0200

    add printing

--- index.js
+++ index.js
@@ -1,24 +1,5 @@
 #! /usr/bin/env node
 var pkg = require('./package.json');
-var program = require('commander');
+var index = require('./lib/index');
 
-program
-  .version(pkg.version)
-  .usage('<source> [options]')
-
-program
-  .command('print')
-  .description('print the specified source')
-  .action(function(source) {
-    var options = program.options.reduce((collection, item) => {
-      if(item.long !== '--version') {
-        var key = item.long.replace(/-/g, '');
-        collection[key] = program[key]
-      }
-      return collection;
-    }, {});
-
-    console.log(options);
-  });
-
-program.parse(process.argv);
+module.exports = index;
